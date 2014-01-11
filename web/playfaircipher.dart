381f28a077b316fa3afb044be9b8e2b97f14bd23
STechAlex <alex@stechstudio.com>
2014-01-11 09:09:55 -0500

    Add comments to document what stuff does

--- web/playfaircipher.dart
+++ web/playfaircipher.dart
@@ -36,16 +36,18 @@ void preCrypt(Event e){
   TextAreaElement originalArea;
   
   if(e.target == querySelector("#plaintextArea")){
-    print("Typing in plaintext area!");
+    // This is done when we're typing in the plaintext area
     originalArea = querySelector("#plaintextArea");
     targetArea = querySelector("#ciphertextArea");
     encrypt = true;
   } else {
-    print('Typing in ciphertext area!');
+    // This is done when we're typing in the ciphertext area
     originalArea = querySelector("#ciphertextArea");
     targetArea = querySelector("#plaintextArea");
     encrypt = false;
   }
+  
+  // Force the typed-in area to be all-caps
   originalArea.value = originalArea.value.toUpperCase();
   crypt(originalArea, targetArea, encrypt);
 }
@@ -58,11 +60,11 @@ void preCrypt(Event e){
  */
 void crypt(TextAreaElement originalArea, TextAreaElement targetArea, bool encrypt){
   if(originalArea == querySelector("#ciphertextArea") && (originalArea.value.length % 2 == 1)){
-    
+    // Do nothing if we're typing in the ciphertext area and we've got an odd number of characters
   } else if(!originalArea.value.isEmpty){
     // Sanitize! Alphabetical characters only!
     var original = originalArea.value.toUpperCase().replaceAll(new RegExp(r"[^a-zA-Z]*"), '');
-    String target = '';
+    String targetStr = '';
 
     // Loop for the length of the string, incrementing by two
     for(var i = 0; i < original.length; i = i + 2){
@@ -73,33 +75,46 @@ void crypt(TextAreaElement originalArea, TextAreaElement targetArea, bool encryp
       String firstLetter = original[i];
       String secondLetter = original[i + 1];
       
-      // print(original); //Debugging
       if(firstLetter == secondLetter){ // If this digraph's letters match, insert an X
         original = original.substring(0, i + 1) + "X" + original.substring(i+1);
       }
-      
-      target = target + square.cryptPair(original[i] + original[i + 1], encrypt);
+      // Encrypt/decrypt our pair
+      targetStr = targetStr + square.cryptPair(original[i] + original[i + 1], encrypt);
     }
-    targetArea.value = target;
+    targetArea.value = targetStr;
   } else {
+    // This is to allow for empty input fields to be updated
     targetArea.value = "";
   }
 }
 
+/**
+ * Event-triggered change of the playfair key 
+ */
 void changeKey(Event e){
   square = new Square(cipherKey.value);
   crypt(querySelector("#plaintextArea"),querySelector("#ciphertextArea"),true);
 }
 
+/**
+ * Event-triggered change of the key field to uppercase on-blur
+ */
 void upperCaseKey(Event e){
   (e.target as InputElement).value = (e.target as InputElement).value.toUpperCase();
 }
 
+/**
+ * Class that manages the Playfair encryption-square
+ */
 class Square {
   List alphabet = new List();
   List columns = new List();
   List rows = new List();
   
+  /**
+   * Creates the columns and rows of the square
+   * @param String key The key for the newly-generated square. This allows you to call new Square("newkey")
+   */
   Square(String key){
     for(var i = 0; i < 5; i++){
       columns.add(new List());
@@ -108,6 +123,10 @@ class Square {
     this.generate(key);
   }
   
+  /**
+   * Generates the string anew. Call this anytime the key is changed
+   * @param String key The key we're generating a square from
+   */
   void generate(String key){
     String newKey = key.toUpperCase();
     var playfairKey = newKey.replaceAll(new RegExp(r'j'),'i').replaceAll(new RegExp(r'J'), 'I') + "ABCDEFGHIKLMNOPQRSTUVWXYZ";
@@ -129,6 +148,9 @@ class Square {
     }
   }
   
+  /**
+   * Encrypts or decrypts a pair of characters using the generated square
+   */
   String cryptPair(String pair, bool encrypt){
     int direction = (encrypt) ? 1 : -1 ;
     String letter1 = pair[0];
@@ -153,6 +175,9 @@ class Square {
     return newLetter1 + newLetter2;
   }
   
+  /**
+   * Get the row of a particular letter in the generated square
+   */
   int getRow(String letter){
     int letterpos = -1;
     int count = 0;
@@ -163,6 +188,9 @@ class Square {
     return letterpos;
   }
 
+  /**
+   * Get the columns of a particular letter in the generated square
+   */
   int getColumn(String letter){
     int letterpos = -1;
     int count = 0;
@@ -173,6 +201,9 @@ class Square {
     return letterpos;
   }
   
+  /**
+   * Retrieves the letter at a position in the square
+   */
   String getLetterAt(int row, int column){
     return this.rows[row][column];
   }
